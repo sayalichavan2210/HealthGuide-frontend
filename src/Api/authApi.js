1a5a5d5ded5fe677eae24222fedeaf57c2c5181c
sayalichavan2210 <sayalic106@gmail.com>
2026-05-20 12:09:15 +0530

    fix

--- src/Api/authApi.js
+++ src/Api/authApi.js
@@ -3,7 +3,7 @@ import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 export const authApi = createApi({
   reducerPath: "authApi",
   baseQuery: fetchBaseQuery({
-    baseUrl: `${import.meta.env.VITE_API_URL}/api/auth`,
+      baseUrl: "https://healthguide-backend.onrender.com/api/auth",
     prepareHeaders: (headers, { getState }) => {
       const token = getState().auth.token;
       if (token) headers.set("Authorization", `Bearer ${token}`);
